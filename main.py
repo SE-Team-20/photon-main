98cc8fdd0fd9e8c9c7fe4178b853fbb11286b75b
TrueRyoB <trueryob@proton.me>
2026-02-03 16:22:07 -0600

    feat: modified both the screen size and the image size

--- main.py
+++ main.py
@@ -1,22 +1,28 @@
 import sys
 
 # UI framework
+from PyQt6.QtCore import Qt, QSize, QTimer
 from PyQt6.QtGui import QPixmap
-from PyQt6.QtWidgets import QApplication, QWidget, QMainWindow, QLabel
+from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel, QSplashScreen
 
 class MainWindow(QMainWindow):
   def __init__(self):
     super().__init__()
-    self.title = "Photon Main (team 20)"
+
+    self.title = "Photon Main - team 20"
     self.setWindowTitle(self.title)
 
+    # self.setGeometry(0, 0, 300, 300)
+
+    self.setFixedSize(300, 300)
+
+
     label=QLabel(self)
-    pixmap=QPixmap('UI_images/logo.jpg')
+    pixmap=QPixmap('UI_images/logo.jpg').scaled(QSize(300, 300))
     label.setPixmap(pixmap)
     self.setCentralWidget(label)
-    self.resize(pixmap.width(), pixmap.height())
 
-    self.statusBar().showMessage("Welcome to " + self.title)
+    self.statusBar().showMessage("splashscreen(" + self.title + ")")
 
 def main():
   print("---main.py is called---")
