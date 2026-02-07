1b335576624118f98069b30a315b6f798612e0da
TrueRyoB <trueryob@proton.me>
2026-02-06 18:37:12 -0600

    feat: basic methods and implementation strategy

--- src/database.py
+++ src/database.py
@@ -1,61 +1,114 @@
 import psycopg2
 from psycopg2 import sql
+from util import (
+  validIndex
+)
 
-# Define connection parameters
+from constants import (
+  DB_NAME,
+  DB_USERNAME,
+  DB_PASSWORD,
+  DB_HOST,
+  DB_PORT,
+  DB_TABLE_PLAYERS,
+  DB_TABLE_EQUIPS,
+  MAX_NUM_PLAYER,
+  NUM_TEAM
+)
+
+class DB:
+  # TODO: return false if failed to make one
+  def __init__(self):
+    self.conn = psycopg2.connect(**connection_params)
+    self.cur = self.conn.cursor()
+    self.create_tables()
+  
+  def close(self):
+    if self.cur:
+      self.cur.close()
+    if self.conn:
+      self.conn.close()
+
+  def create_tables(self):
+    # Scheme "player ID - codename - team ID - score"
+    # (team ID is ommited to take advantage of modulo property)
+    self.cur.execute(sql.SQL(
+      '''
+      CREATE TABLE IF NOT EXISTS {} (
+        player_id SERIAL PRIMARY KEY,
+        codename TEXT,
+        score NUMERIC DEFAULT 0,
+        is_registered BOOLEAN DEFAULT FALSE
+      )
+      WITH (fillfactor = {});
+     '''
+    ).format(
+      sql.Identifier(DB_TABLE_PLAYERS),
+      sql.Literal(MAX_NUM_PLAYER*NUM_TEAM) 
+    ))
+
+    # Scheme "player ID - equipment ID"
+    self.cur.execute(sql.SQL(
+      '''
+      CREATE TABLE IF NOT EXISTS {} (
+        player_id INTEGER 
+          REFERENCES {}(player_id) 
+          ON DELETE CASCADE,
+        equip_id INTEGER,
+        PRIMARY KEY (player_id, equip_id)
+      );
+     '''
+    ).format(
+      sql.Identifier(DB_TABLE_EQUIPS),
+      sql.Identifier(DB_TABLE_PLAYERS)
+    ))
+  
+  def getIndex(player_id: int, team_id: int):
+    return player_id+team_id*MAX_NUM_PLAYER
+  
+  def set_player(self, player_id: int, codename: str, team_id: int):
+    if not validIndex(player_id, MAX_NUM_PLAYER) or not validIndex(team_id, NUM_TEAM):
+      return False
+    
+    # disable the row if player_id is at size 0
+    registered = "TRUE" if len(player_id)>0 else "FALSE"
+
+    self.cur.execute(sql.SQL(
+      '''
+      UPDATE players
+      SET codename = '{}',
+        is_registered = {}
+      WHERE player_id = {};
+      '''
+    ).format(
+      sql.Identifier(codename),
+      sql.Identifier(registered),
+      sql.Literal(self.getIndex(player_id, team_id))
+    ))
+
+  # TODO: Update score (change, playerID, teamID)
+
+  # TODO: Get ranking (teamID)
+  
+
 connection_params = {
-    "dbname": "photon",
-    "user": "student",
-    #'password': 'student',
-    #'host': 'localhost',
-    #'port': '5432'
+  "dbname": f"{DB_NAME}",
+  "user": f"{DB_USERNAME}",
+  'password': f"{DB_PASSWORD}",
+  'host': f"{DB_HOST}",
+  'port': f"{DB_PORT}",
 }
 
-try:
-    # Connect to PostgreSQL
-    conn = psycopg2.connect(**connection_params)
-    cursor = conn.cursor()
+# wrapper/validator (do we need it?????)
+def run_query(sql_text, params=None, fetch=False):
+  with psycopg2.connect(**connection_params) as conn:
+     with conn.cursor() as cur:
+        cur.execute(sql_text, params)
+        if fetch:
+           return cur.fetchall()
 
-    # Execute a query
-    cursor.execute("SELECT version();")
-
-    # Fetch and display the result
-    version = cursor.fetchone()
-    print(f"Connected to - {version}")
-
-    # Example: creating a table
-    # cursor.execute('''
-    #    CREATE TABLE IF NOT EXISTS employees (
-    #        id SERIAL PRIMARY KEY,
-    #        name VARCHAR(100),
-    #        department VARCHAR(50),
-    #        salary DECIMAL
-    #    );
-    #''')
-
-    # Insert sample data
-    cursor.execute(
-        """
-        INSERT INTO players (id, codename)
-        VALUES (%s, %s);
-    """,
-        ("500", "BhodiLi"),
-    )
-
-    # Commit the changes
-    conn.commit()
-
-    # Fetch and display data from the table
-    cursor.execute("SELECT * FROM players;")
-    rows = cursor.fetchall()
-    for row in rows:
-        print(row)
-
-except Exception as error:
-    print(f"Error connecting to PostgreSQL database: {error}")
-
-finally:
-    # Close the cursor and connection
-    if cursor:
-        cursor.close()
-    if conn:
-        conn.close()
+# hints
+  # conn.commit()
+  # rows = cursor.fetchall()
+  # for row in rows:
+  #   print(row)
---
+++ src/util.py
@@ -0,0 +1,2 @@
+def validIndex(val: int, len: int):
+  return val>=0 and val<len
