8571e7820c44d4751176889b8cd2235ed981ade9
Wahid Sultani <sultaniwahid240@gmail.com>
2026-02-09 19:43:37 -0600

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,11 @@
-# chmod +x run.sh
-#./run.sh#!/bin/bash
+#!/bin/bash
+# Launcher script for Photon Laser Tag System
+# Make the file executable: chmod +x run.sh
+# Run the file : ./run.sh
+
+# Activate Python virtual environment
 source venv/bin/activate
+
+# Run main program
 python3 main.py
 
