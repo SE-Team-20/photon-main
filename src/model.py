97975e17bf0235fc30a0bfc8d9abbff2d4708035
TrueRyoB <trueryob@proton.me>
2026-02-09 21:51:03 -0600

    feat: added specific columns to be addressed with

--- src/model.py
+++ src/model.py
@@ -6,9 +6,16 @@
 # =================================
 # TODO: Parse input
 # input: ref of data, ref of DB instance
-# output: True / a pair of {error statements, a list of indices of input fields that needs to be fixed}
+# output: True / a pair of {an error statement, a list of indices of input fields that needs to be fixed}
 
 
+# Lists of errors
+
+# equipment ID remaining blank
+# codename blank but equip ID filled
+# not enough participants (at least 1 for each team)
+# unbalanced # of teammates
+# duplicated codenames
 
 
 # =================================
