2d23c0f3919fb46d1fdde4effba467a8ed51d7b6
TrueRyoB <trueryob@proton.me>
2026-02-09 21:40:57 -0600

    init

---
+++ src/model.py
@@ -0,0 +1,18 @@
+# Handles events from UDP by calling database and update the GUI
+
+
+# =================================
+# Pre-game events
+# =================================
+# TODO: Parse input
+# input: ref of data, ref of DB instance
+# output: True / a pair of {error statements, a list of indices of input fields that needs to be fixed}
+
+
+
+
+# =================================
+# In-game events
+# =================================
+# TODO: Handles signals
+
