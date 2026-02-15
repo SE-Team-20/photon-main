8745ee07b889839e64aefa023618512453e99f75
TrueRyoB <trueryob@proton.me>
2026-02-15 17:20:50 -0600

    fix: re-write the database logics to meet the need

--- src/database.py
+++ src/database.py
@@ -40,19 +40,12 @@ class DB:
       print("DB error:", e)
       return False
 
-  def close(self):
-    if self.cur:
-      self.cur.close()
-    if self.conn:
-      self.conn.close()
-    self.cur=None
-    self.conn=None
-
   # Scheme "player ID - codename - team ID - score"
+  # TODO: 
   def _create_table(self):
     self._ensure_db()
 
-# TODO: change a scheme and add "game session ID" to keep track of the player pool for the specific match
+    # TODO: change a scheme and add "game session ID" to keep track of the player pool for the specific match
     self._safe_exec(sql.SQL(
       '''
       CREATE TABLE IF NOT EXISTS players (
@@ -67,25 +60,15 @@ class DB:
     ).format(
       sql.Literal(MAX_NUM_PLAYER*NUM_TEAM) 
     ))
-  
-  # assign a codename to a specific player id row
-  # TODO: make it register to the buffer on memory instead
-  def set_player(self, row_index: int, team_id: int, codename: str):
-    self._ensure_db()
-    if not validIndex(team_id, NUM_TEAM):
-      return False
-  
-  # TODO: 
-  def assign_equip(self, row_index: int, team_id: int, equip_id: int):
-    self._ensure_db()
-    if not validIndex(team_id, NUM_TEAM):
-      return False
-    return False
 
-  # update the player score by a difference to the previous score
-  def update_score(self, diff:int, player_id:int):
-    self._ensure_db()
-    return False
+  def close(self):
+    if self.cur:
+      self.cur.close()
+    if self.conn:
+      self.conn.close()
+    self.cur=None
+    self.conn=None
+
 
   # returns a tuple of {rank, codename, score} in non-decreasing order
   def get_leaderboard(self, team_id:int):
@@ -115,19 +98,4 @@ class DB:
     for i in range(1, len(res)):
       res[i][0]= res[i-1][0] if res[i][2]==res[i-1][2] else i+1
     return [tuple(r) for r in res]
-  
-  # TODO: returns a player ID
-  def get_player_info(self, equip_id: int):
-    return False
-  
-  # TODO: 
-  def free_equipment(self, equip_id:int):
-    return False
 
-  # TODO: 
-  def free_equips(self):
-    return False
-  
-  # TODO: 
-  def clear_buffer(self):
-    return False
