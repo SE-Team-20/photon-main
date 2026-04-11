ac142b95ad4c0d704c87b39eb3efc9b093501540
TrueRyoB <trueryob@proton.me>
2026-04-11 15:06:19 -0500

    feat: python file for running both traffic-generator and main in a single terminal window

--- src/traffic-generator.py
+++ src/traffic-generator.py
@@ -12,10 +12,16 @@ print("this program will generate some test traffic for 2 players on the red ")
 print("team as well as 2 players on the green team")
 print("")
 
-red1 = input("Enter equipment id of red player 1 ==> ")
-red2 = input("Enter equipment id of red player 2 ==> ")
-green1 = input("Enter equipment id of green player 1 ==> ")
-green2 = input("Enter equipment id of green player 2 ==> ")
+# red1 = input("Enter equipment id of red player 1 ==> ")
+# red2 = input("Enter equipment id of red player 2 ==> ")
+# green1 = input("Enter equipment id of green player 1 ==> ")
+# green2 = input("Enter equipment id of green player 2 ==> ")
+
+red1 = "1"
+red2 = "3"
+green1 = "2"
+green2 = "4"
+
 
 # Create datagram sockets
 UDPServerSocketReceive = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
