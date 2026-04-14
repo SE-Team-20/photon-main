3fe8e6e142b12b686f20e1629e9454fb6ad06b33
Wahid Sultani <sultaniwahid240@gmail.com>
2026-04-13 23:20:15 -0500

    Update mac_install.sh

--- mac_install.sh
+++ mac_install.sh
@@ -4,7 +4,8 @@ set -e
 echo "--- Installing System Dependencies ---"
 sudo apt update
 sudo apt install -y python3 python3-pip python3-venv build-essential \
-python3-dev python3-pyqt6 libpq-dev postgresql-client dos2unix
+python3-dev python3-pyqt6 python3-pyqt6.qtmultimedia \
+libpq-dev postgresql-client dos2unix
 
 echo "--- Converting scripts to Unix format ---"
 for f in install.sh run.sh; do
